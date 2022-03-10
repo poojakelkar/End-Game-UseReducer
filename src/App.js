e9f94cf3dc3d9896eac22386213e49215b63734d
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-10 22:13:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,173 @@
+import React, { useReducer } from "react";
+import "./styles.css";
+
+import faker from "faker";
+
+faker.seed(123);
+
+const data = [...Array(50)].map((item) => ({
+  id: faker.random.uuid(),
+  name: faker.commerce.productName(),
+  image: faker.random.image(),
+  price: faker.commerce.price(),
+  material: faker.commerce.productMaterial(),
+  brand: faker.lorem.word(),
+  inStock: faker.random.boolean(),
+  fastDelivery: faker.random.boolean(),
+  ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  offer: faker.random.arrayElement([
+    "Save 50",
+    "70% bonanza",
+    "Republic Day Sale"
+  ]),
+  idealFor: faker.random.arrayElement([
+    "Men",
+    "Women",
+    "Girl",
+    "Boy",
+    "Senior"
+  ]),
+  level: faker.random.arrayElement([
+    "beginner",
+    "amateur",
+    "intermediate",
+    "advanced",
+    "professional"
+  ]),
+  color: faker.commerce.color()
+}));
+
+export default function App() {
+  // const [{ sortBy }, dispatch] = useReducer(
+  //   function reducer(state, action) {
+  //     switch (action.type) {
+  //       case "SORT":
+  //         return {
+  //           ...state,
+  //           sortBy: action.payload
+  //         };
+  //       default:
+  //         return state;
+  //     }
+  //   },
+  //   {
+  //     sortBy: null
+  //   }
+  // );
+
+  // function getSortedData(productList, sortBy) {
+  //   if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
+  //     return productList.sort((a, b) => b["price"] - a["price"]);
+  //   }
+
+  //   if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
+  //     return productList.sort((a, b) => a["price"] - b["price"]);
+  //   }
+  //   return productList;
+  // }
+  // getSortedData(data, sortBy);
+
+  const [{ sortBy }, dispatch] = useReducer(
+    function reducer(state, action) {
+      switch (action.type) {
+        case "SORT":
+          return {
+            ...state,
+            sortBy: action.payload
+          };
+        default:
+          return state;
+      }
+    },
+    {
+      sortBy: null
+    }
+  );
+
+  function getSortedData(productList, sortBy) {
+    if (sortBy && sortBy === "PRICE_HIGH_TO_LOW") {
+      return productList.sort((a, b) => b["price"] - a["price"]);
+    }
+
+    if (sortBy && sortBy === "PRICE_LOW_TO_HIGH") {
+      return productList.sort((a, b) => a["price"] - b["price"]);
+    }
+  }
+
+  getSortedData(data, sortBy);
+
+  return (
+    <>
+      <fieldset>
+        <legend>Sort By</legend>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onChange={() =>
+              dispatch({ type: "SORT", payload: "PRICE_HIGH_TO_LOW" })
+            }
+          ></input>{" "}
+          Price-High to Low
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="sort"
+            onChange={() =>
+              dispatch({ type: "SORT", payload: "PRICE_LOW_TO_HIGH" })
+            }
+          ></input>
+          Price-Low to High
+        </label>
+      </fieldset>
+
+      <fieldset style={{ marginTop: "1rem" }}>
+        <legend>Filters</legend>
+        <label>
+          <input type="checkbox"></input>Include Out of Stock
+        </label>
+        <label>
+          <input type="checkbox"></input>Fast Delivery Only
+        </label>
+      </fieldset>
+      <div className="App" style={{ display: "flex", flexWrap: "wrap" }}>
+        {data.map(
+          ({
+            id,
+            name,
+            image,
+            price,
+            productName,
+            inStock,
+            level,
+            fastDelivery
+          }) => (
+            <div
+              key={id}
+              style={{
+                border: "1px solid #4B5563",
+                borderRadius: "0 0 0.5rem 0.5rem",
+                margin: "1rem",
+                maxWidth: "40%",
+                padding: "0 0 1rem"
+              }}
+            >
+              <img src={image} width="100%" height="auto" alt={productName} />
+              <h3> {name} </h3>
+              <div>Rs. {price}</div>
+              {inStock && <div> In Stock </div>}
+              {!inStock && <div> Out of Stock </div>}
+              <div>{level}</div>
+              {fastDelivery ? (
+                <div> Fast Delivery </div>
+              ) : (
+                <div> 3 days minimum </div>
+              )}
+            </div>
+          )
+        )}
+      </div>
+    </>
+  );
+}
